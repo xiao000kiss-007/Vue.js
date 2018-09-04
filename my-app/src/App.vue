5bdf617e31576e8073578252fd3c89b6ba4a6eec
xiao000kiss-007 <48586872@qq.com>
2018-09-04 18:23:23 +0800

    “代码提交信息”

---
+++ my-app/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h2>首页</h2>
+  </div>
+</template>
+
+<script>
+import KAdmin from './components/KAdmin.vue'
+
+export default {
+  name: 'app',
+  components: {
+    KAdmin
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+#app {
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif; */
+  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  a{
+    text-decoration: none;
+    font-size: 14px;
+    color: aliceblue;
+  }
+}
+</style>
---
+++ my-app/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+       <h2>首页</h2>
+        <div>
+          <div class="grid-content bg-purple-dark">
+           <img src="../assets/logo.png">
+        </div>
+      </div>
+    <h1>欢迎来到KAdmin管理系统v1.0.0</h1>
+    <el-row>
+      <el-button type="primary"><router-link to="/kadmin">进入系统</router-link></el-button>
+     <router-view></router-view>
+    </el-row>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import KAdmin from '@/components/KAdmin.vue'
+
+export default {
+  name: 'home',
+  components: {
+    KAdmin
+  }
+}
+</script>
